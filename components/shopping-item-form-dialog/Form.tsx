2f0010646db0c54fabd152e63460b128b12cc3f2
ahmod001 <ahmodh08@gmail.com>
2025-06-09 14:38:21 +0600

    _

--- components/shopping-item-form-dialog/Form.tsx
+++ components/shopping-item-form-dialog/Form.tsx
@@ -1,15 +1,45 @@
 import React from 'react'
 import { View } from 'react-native'
 import TextField from '../form/TextField'
+import * as yup from 'yup'
+import { Controller, useForm, } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup';
+import ReactHookFormController from '@/services/react-hook-form/components/ReactHookFormController'
+
+const schema = yup.object({
+    title: yup.string().required('পন্যের নাম দিতে হবে'),
+    amount: yup.string().required('পন্যের পরিমান দিতে হবে')
+})
 
 const Form = () => {
-    const _e = ''
+    const { control, handleSubmit } = useForm({
+        mode: 'onBlur',
+        defaultValues: { title: '', amount: '' },
+        resolver: yupResolver(schema)
+    })
     return (
         <View className='gap-y-1'>
-            <TextField autoFocus label='পন্যের নাম' placeholder='যেমন আলু' error={_e} />
-            <TextField label='পন্যের পরিমান' placeholder='যেমন ১ kg' error={_e} />
+            <ReactHookFormController
+                control={control}
+                name="title">
+                <TextField
+                    label="পণ্যের নাম"
+                    placeholder="যেমনঃ আলু"
+                />
+            </ReactHookFormController>
+
+
+            <ReactHookFormController
+                control={control}
+                name="amount">
+                <TextField label='পন্যের পরিমান' placeholder='যেমন ১ kg' />
+            </ReactHookFormController>
+
         </View>
     )
 }
+
+
+
 
 export default Form
---
+++ services/react-hook-form/components/ReactHookFormController.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { type Control, Controller } from 'react-hook-form'
+
+
+
+interface Props {
+    name: string,
+    control: Control<any>,
+    children: React.ReactElement<any>
+}
+
+const ReactHookFormController = ({ children, name, control }: Props) => {
+    return (
+        <Controller
+            control={control}
+            name={name}
+            render={({ field, formState: { errors } }) => (
+                React.cloneElement(children, {
+                    ...field,
+                    error: errors?.[name]?.message
+                })
+
+            )
+            }
+        />
+    )
+}
+
+export default ReactHookFormController
